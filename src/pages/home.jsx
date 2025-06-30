cbac11e96292f1d788c486e98dd2af565f888358
Morad Shebl <moradsheblpaik@gmail.com>
2025-06-30 19:52:38 +0300

    Fix logo

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -131,7 +131,60 @@ function Home() {
               </svg>
             </button>
             <div className="logo">
-              <img src="src\assets\Favicon.png" alt="Logo" />
+              <svg
+                version="1.1"
+                xmlns="http://www.w3.org/2000/svg"
+                xmlnsXlink="http://www.w3.org/1999/xlink"
+                width="396.3px"
+                height="411.4px"
+                viewBox="0 0 396.3 411.4"
+                xmlSpace="preserve"
+              >
+                <defs>
+                  <linearGradient
+                    id="SVGID_1_"
+                    gradientUnits="userSpaceOnUse"
+                    x1="148.6643"
+                    y1="334.2465"
+                    x2="147.1253"
+                    y2="419.6609"
+                  >
+                    <stop offset="0" stopColor="#FFFFFF"></stop>
+                    <stop offset="1" stopColor="#000000"></stop>
+                  </linearGradient>
+                  <linearGradient
+                    id="SVGID_2_"
+                    gradientUnits="userSpaceOnUse"
+                    x1="284.4213"
+                    y1="121.228"
+                    x2="335.9778"
+                    y2="81.2141"
+                  >
+                    <stop offset="0" stopColor="#FFFFFF"></stop>
+                    <stop offset="1" stopColor="#000000"></stop>
+                  </linearGradient>
+                </defs>
+                <g>
+                  <path
+                    className="st0"
+                    style={{ fill: "#FFFFFF" }}
+                    d="M283.3,136.1c12.2-6.8,24.3-13.4,36.2-20.1L283.3,136.1z M132.6,192.9v126.8l31.6,4.1V206.5
+                c33.1-22.1,69.8-43,106.1-63.2l-14.9-21.6L132.6,192.9z M197.4,0L0,128.1V327l153.7,84.4L396.3,333V128.1L197.4,0z M33.2,308.9
+                V149.2l164.3-110c36.1,23.1,76.5,47.7,95.7,59.8l0.8,0.5l-38.4,22.2l-122.8,71.2v165.8L33.2,308.9z M361.7,310.4l-195.9,64.8
+                l-0.5-0.3l-1-0.5v-168c33.1-22.1,69.8-43,106.1-63.2l12.9-7.2c12.2-6.8,24.3-13.4,36.2-20.1c22.2,14,29.9,19.4,42.2,27.1V310.4z"
+                  ></path>
+                  <polygon
+                    className="st1"
+                    style={{ fill: "url(#SVGID_1_)" }}
+                    points="164.3,323.8 164.3,374.5 132.6,358.7 132.6,319.7"
+                  ></polygon>
+                  <polygon
+                    className="st2"
+                    style={{ fill: "url(#SVGID_2_)" }}
+                    points="319.5,116 283.3,136.1 270.3,143.3 255.5,121.7 293.8,99.5"
+                  ></polygon>
+                </g>
+              </svg>
               <span>Critic</span>
             </div>
             <nav>
