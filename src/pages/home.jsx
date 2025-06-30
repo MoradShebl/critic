f162b9a9ef1f60d672d6abdf3778719012f12340
Morad Shebl <moradsheblpaik@gmail.com>
2025-06-30 19:49:20 +0300

    add logo

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -131,7 +131,7 @@ function Home() {
               </svg>
             </button>
             <div className="logo">
-              <img src="public\Favicon.png" alt="Logo" />
+              <img src="src\assets\Favicon.png" alt="Logo" />
               <span>Critic</span>
             </div>
             <nav>
